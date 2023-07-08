10eb3debc5bc339e60cff0c853e6006c694c2d6d
Lyonga <60431189+Lyonga@users.noreply.github.com>
2023-07-08 12:09:06 -0400

    Delete backend.tf

--- aws/terraform/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "charlyoinfotect.cm"
-    key            = "path/to/terraform.tfstate"
-    region         = "us-east-1"
-    encrypt        = true
-    #dynamodb_table = "your-lock-table"
-  }
-}
