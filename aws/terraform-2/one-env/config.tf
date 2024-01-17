23dba1cc6babebc49b2fd56b9e24b1bff4568729
clyonga <charles.lyonga1@accesscfa.com>
2024-01-17 13:08:46 -0500

    add aws config files

--- aws/terraform-2/one-env/config.tf
+++ aws/terraform-2/one-env/config.tf
@@ -98,23 +98,23 @@ resource "aws_s3_bucket" "config_bucket" {
 
 }
 
-resource "aws_s3_bucket_server_side_encryption_configuration" "bucket-encryption" {
-  bucket = aws_s3_bucket.config_bucket.bucket
-
-  rule {
-    apply_server_side_encryption_by_default {
-      sse_algorithm = "AES256"
-    }
-  }
-}
-
-resource "aws_s3_bucket_public_access_block" "config_bucket" {
-  bucket = aws_s3_bucket.config_bucket.id
-
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
-  depends_on = [aws_s3_bucket.config_bucket]
-}
+#resource "aws_s3_bucket_server_side_encryption_configuration" "bucket-encryption" {
+#  bucket = aws_s3_bucket.config_bucket.bucket
+#
+#  rule {
+#    apply_server_side_encryption_by_default {
+#      sse_algorithm = "AES256"
+#    }
+#  }
+#}
+#
+#resource "aws_s3_bucket_public_access_block" "config_bucket" {
+#  bucket = aws_s3_bucket.config_bucket.id
+#
+#  block_public_acls       = true
+#  block_public_policy     = true
+#  ignore_public_acls      = true
+#  restrict_public_buckets = true
+#  depends_on = [aws_s3_bucket.config_bucket]
+#}
 //test new
