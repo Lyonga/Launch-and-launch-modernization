180d57d748a61dad5a6883b8896ba6b37c5080f1
Lyonga <60431189+Lyonga@users.noreply.github.com>
2024-07-24 18:32:19 -0400

    Update Load-Balancer.tf

--- aws/terraform/Load-Balancer.tf
+++ aws/terraform/Load-Balancer.tf
@@ -1,5 +1,5 @@
 resource "aws_lb" "LB" {
-  name               = "Main--test-lb"
+  name               = "Main--test-alb"
   internal           = false
   load_balancer_type = "application"
   security_groups    = [aws_security_group.SG.id]
