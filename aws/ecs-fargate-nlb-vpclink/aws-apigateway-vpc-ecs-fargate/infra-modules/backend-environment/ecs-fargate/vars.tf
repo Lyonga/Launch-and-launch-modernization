5815dc081c00718ee06b10b03599503b2163c849
clyonga <charles.lyonga1@accesscfa.com>
2024-08-02 09:39:03 -0400

    cleanup

--- aws/ecs-fargate-nlb-vpclink/aws-apigateway-vpc-ecs-fargate/infra-modules/backend-environment/ecs-fargate/vars.tf
+++ aws/ecs-fargate-nlb-vpclink/aws-apigateway-vpc-ecs-fargate/infra-modules/backend-environment/ecs-fargate/vars.tf
@@ -10,7 +10,7 @@ variable "app_image" {
 
 variable "environment" {
   type = string
-  description = "amplifier test environment"
+  description = "amplifier environment"
 }
 
 variable "fargate_cpu" {
