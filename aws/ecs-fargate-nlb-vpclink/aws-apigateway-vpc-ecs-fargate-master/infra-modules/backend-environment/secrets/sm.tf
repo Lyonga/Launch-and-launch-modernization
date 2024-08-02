7e524ac86b955a3c2100f16904a70133727cae29
clyonga <charles.lyonga1@accesscfa.com>
2024-08-02 09:12:45 -0400

    redeploy

--- aws/ecs-fargate-nlb-vpclink/aws-apigateway-vpc-ecs-fargate-master/infra-modules/backend-environment/secrets/sm.tf
+++ aws/ecs-fargate-nlb-vpclink/aws-apigateway-vpc-ecs-fargate-master/infra-modules/backend-environment/secrets/sm.tf
@@ -1,17 +1,17 @@
-resource "aws_secretsmanager_secret" "amplifier-secret" "{
-  name = "username"
-}
+# resource "aws_secretsmanager_secret" "amplifier-secret" "{
+#   name = "username"
+# }
 
-resource "aws_secretsmanager_secret_version" "amplifier-secret-version" {
-  secret_id     = aws_secretsmanager_secret.amplifier-secret.id
-  secret_string = "test"
-}
+# resource "aws_secretsmanager_secret_version" "amplifier-secret-version" {
+#   secret_id     = aws_secretsmanager_secret.amplifier-secret.id
+#   secret_string = "test"
+# }
 
-resource "aws_secretsmanager_secret" "amplifier" {
-  name = "password"
-}
+# resource "aws_secretsmanager_secret" "amplifier" {
+#   name = "password"
+# }
 
-resource "aws_secretsmanager_secret_version" "password_secret" {
-  secret_id     = aws_secretsmanager_secret.amplifier.id
-  secret_string = "testp"
-}
+# resource "aws_secretsmanager_secret_version" "password_secret" {
+#   secret_id     = aws_secretsmanager_secret.amplifier.id
+#   secret_string = "testp"
+# }
