33039c04c7c994e0c6807fc34991ae5986de14aa
clyonga <charles.lyonga1@accesscfa.com>
2024-08-08 15:47:07 -0400

    added values

--- aws/ecs-fargate-nlb-vpclink/aws-apigateway-vpc-ecs-fargate/infra-modules/backend-environment/variables.tf
+++ aws/ecs-fargate-nlb-vpclink/aws-apigateway-vpc-ecs-fargate/infra-modules/backend-environment/variables.tf
@@ -11,6 +11,7 @@ variable "profile" {
 variable "region" {
   description = "aws region to deploy to"
   type        = string
+  default = us-east-1
 }
 
 variable "platform_name" {
@@ -21,6 +22,7 @@ variable "platform_name" {
 variable "environment" {
   description = "Applicaiton environment"
   type = string
+  default = dev
 }
 
 variable "app_port" {
@@ -39,8 +41,9 @@ variable "availability_zones" {
 }
 
 variable "app_count" {
-  type = string 
+  type = number 
   description = "The number of instances of the task definition to place and keep running."
+  default = 1
 }
 
 /* variable "main_pvt_route_table_id" {
