3287f6f091a0e3f8829cbe59bc6e74105b928290
clyonga <charles.lyonga1@accesscfa.com>
2024-08-08 15:36:43 -0400

    update workflow

--- aws/ecs-fargate-nlb-vpclink/aws-apigateway-vpc-ecs-fargate/infra-modules/backend-environment/ecs-fargate/vars.tf
+++ aws/ecs-fargate-nlb-vpclink/aws-apigateway-vpc-ecs-fargate/infra-modules/backend-environment/ecs-fargate/vars.tf
@@ -11,7 +11,7 @@ variable "app_image" {
 
 variable "environment" {
   type = string
-  description = "amplifier test environment"
+  description = "amplifier tes environment"
 }
 
 variable "fargate_cpu" {
